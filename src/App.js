1fa8aba0cdf2030cab65406685401d33c7c01787
Nathaniel <nathjoeetim@gmail.com>
2023-02-22 16:06:53 +0100

    fotter1.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import "./App.css";
 import Home from "./pages/Home";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Home />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
+// function App() {
+//   return (
+//     <div>
+//       <Home />
+//     </div>
+//   );
+// }
 
 export default App;
